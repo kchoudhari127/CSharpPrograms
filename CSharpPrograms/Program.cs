92713277a030e7073117fda69f06b0f74ed64093
kchoudhari127 <kchoudhari127@gmail.com>
2024-07-25 01:29:12 +0530

    C# Programs

---
+++ CSharpPrograms/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Practic_Codes;
+
+Console.WriteLine("Hello, World!");
+
+Multi_Catch_Block obj = new Multi_Catch_Block();
+
+obj.Multi_Catch_Block_Emp();
---
+++ Practic_Codes/Multi_Catch_Block.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic_Codes
+{
+    //Q.Can we use multiple catch blocks in c# ?
+
+    //Ans ==> Yes, in C#, We can use multiple catch blocks for a given try block.
+    //However, at any given point, only one catch block will be executed, and the others will be ignored.
+    //In this example, we have three catch blocks: one for DivideByZeroException, FormatException and another for others Exceptions .
+    //Depending on the exception thrown, the appropriate catch block will handle it.
+
+    public class Multi_Catch_Block
+    {
+        public void Multi_Catch_Block_Emp()
+        {
+            try
+            {
+                Console.WriteLine("Enter number1:");
+                int number1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter number2:");
+                int number2 = int.Parse(Console.ReadLine());
+
+                int result = number1 / number2;
+
+                Console.WriteLine("result: "+result);
+                 
+
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+
+        }
+    }
+}
